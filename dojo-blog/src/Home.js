cc896c8c16e3a630b291a8e95d506c717880a46e
AprilXS <67018736+AprilXS@users.noreply.github.com>
2023-04-30 23:45:36 +0000

    finish lesson-17

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,36 +1,24 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+  const [blogs, setBlogs] = useState(null)
 
-  const [name, setName] = useState('mario');
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
-
-  // useEffect hook
-   useEffect(() => {
-     console.log('use effect ran');
-   }, [name]);
-
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data);
+      })
+  }, [])
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => {setName('luigi')}}>Change name</button>
-      <p>{ name }</p>
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 }
  
 export default Home;
-
-// q: the difference between a useState hook and a useEffect hook?
-// a: useState is used to store a value in a component, and useEffect is used to do something after render.
