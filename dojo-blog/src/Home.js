0d914a3ff2018321c988f8f58390ec3e84253761
AprilXS <67018736+AprilXS@users.noreply.github.com>
2023-04-30 23:25:46 +0000

    finish lesson-15

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
+  const [name, setName] = useState('mario');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
@@ -16,12 +18,14 @@ const Home = () => {
   // useEffect hook
    useEffect(() => {
      console.log('use effect ran');
-   });
+   }, [name]);
 
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => {setName('luigi')}}>Change name</button>
+      <p>{ name }</p>
     </div>
   );
 }
